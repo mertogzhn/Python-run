ba5701da443f80c6c2dfa2511c0c2777404c31b8
Mert Oğuzhanasiltürk <mert.oguzhanasilturk@gmail.com>
2022-03-16 16:38:43 +0300

    modul

---
+++ modul/main.py
@@ -0,0 +1,18 @@
+import mod
+
+#result = help(mod)
+
+value=mod.numbers
+
+print(value)
+
+result=mod.person["name"]
+
+print(result)
+
+result=mod.func(10)
+
+
+p=mod.Person()
+
+p.speak()
---
+++ modul/mod.py
@@ -0,0 +1,22 @@
+"""
+Modül hakkinda genel bilgilendirme
+"""
+
+print("modül eklendi")
+
+number =10
+
+numbers=[1,2,3,]
+
+person={
+    "name":"Ali",
+    "age":"25",
+    "city":"istanbul"
+}
+
+def func(x):
+    print(f"x: {x}")
+
+class Person:
+    def speak(self):
+        print("I am speaking...")
